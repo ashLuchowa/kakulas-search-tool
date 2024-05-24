c82cdb018f0ce1acd076b299649f26c1e3a5bc0b
ashLuchowa <ashwinluchowa@gmail.com>
2024-05-24 12:10:47 +0800

    Add item array library + product variables

--- js/main.js
+++ js/main.js
@@ -8,3 +8,12 @@ class Products {
         this.id = id
     }
 }
+
+// Generate each items
+const chalkLicorice = new Products('Chalk Licorice', 'Licorice that looks like a white chalk', '', '18', 'confect', '1011' );
+const glaceGinger = new Products('Glace Ginger', 'Ginger in syrup juice', '', '29', 'confect', '1012' );
+const almondGrade1 = new Products('Almond Grade 1', 'First grade almond', '', '15.80', 'nuts', '1013' );
+const limaBeans = new Products('Lima Beans', 'White looking beans', '', '28.85', 'beans', '1014' );
+
+// Item Library
+const items = [chalkLicorice, glaceGinger, almondGrade1, limaBeans];
