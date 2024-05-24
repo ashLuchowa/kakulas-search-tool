a8140afc9e070cac668e5502a62a50989c1dc5b5
ashLuchowa <ashwinluchowa@gmail.com>
2024-05-24 14:55:46 +0800

    Added 2nd grade almond + Regex pattern

--- js/main.js
+++ js/main.js
@@ -10,13 +10,14 @@ class Products {
 }
 
 // Generate each items
-const chalkLicorice = new Products('Chalk Licorice', 'Licorice that looks like a white chalk', '', '18', 'confect', '1011' );
+const chalkLicorice = new Products('Chalk Licorice', 'Licorice that looks like a white chalk', '', '18', ['chalk', 'white', 'licorice'], '1011' );
 const glaceGinger = new Products('Glace Ginger', 'Ginger in syrup juice', '', '29', 'confect', '1012' );
 const almondGrade1 = new Products('Almond Grade 1', 'First grade almond', '', '15.80', 'nuts', '1013' );
-const limaBeans = new Products('Lima Beans', 'White looking beans', '', '28.85', 'beans', '1014' );
+const almondGrade2 = new Products('Almond Grade 2', 'Second grade almond', '', '11.50', 'nuts', '1014' );
+const limaBeans = new Products('Lima Beans', 'White looking beans', '', '28.85', 'beans', '1015' );
 
 // Item Library
-const items = [chalkLicorice, glaceGinger, almondGrade1, limaBeans];
+const items = [chalkLicorice, glaceGinger, almondGrade1, almondGrade2, limaBeans];
 
 // Search Functions
 function searchMain() {
@@ -37,12 +38,13 @@ function displayContent(searchContent) {
         mainContainer.appendChild(itemContainer);
     });
 
-    // Filter using texts
-    const filteredItemsDescription = items.filter(item => item.title === searchContent);
+    // Filter using texts and Regex
+    const regexPattern = new RegExp(searchContent, 'i');
+    const filteredItemsDescription = items.filter(item => regexPattern.test(item.tags) || regexPattern.test(item.title));
     filteredItemsDescription.forEach(item => {
         const itemContainer = document.createElement('div');
         itemContainer.textContent = item.title;
-        mainContainer.appendChild(itemContainer);
+        mainContainer.appendChild(itemContainer); 
     });
 }
 
