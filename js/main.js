288a9a2277abc8461147f1e53c99fc60a0894e05
ashLuchowa <ashwinluchowa@gmail.com>
2024-05-24 15:45:30 +0800

    Remove display with tag function + Improve arrays

--- js/main.js
+++ js/main.js
@@ -6,13 +6,16 @@ class Products {
         this.price = price
         this.tags = tags
     }
+    titleColor() {
+        return `${this.tags[this.tags.length - 1]} ${this.tags[0]}`;
+    }
 }
 
 // Generate each items
 const chalkLicorice = new Products('Chalk Licorice', 'Licorice that looks like a white chalk', '', '18', ['confect', 'chalk', 'licorice', 'white']);
-const glaceGinger = new Products('Glace Ginger', 'Ginger in syrup juice', '', '29', ['confect', 'glace', 'ginger', 'syrup, yellow']);
+const glaceGinger = new Products('Glace Ginger', 'Ginger in syrup juice', '', '29', ['confect', 'glace', 'ginger', 'yellow']);
 const almondGrade1 = new Products('Almond Grade 1', 'First grade almond', '', '15.80', ['almond1', 'grade', '1', 'brown']);
-const almondGrade2 = new Products('Almond Grade 2', 'Second grade almond', '', '11.50', ['almond2', 'glade', '2', 'brown']);
+const almondGrade2 = new Products('Almond Grade 2', 'Second grade almond', '', '11.50', ['almond2', 'grade', '2', 'brown']);
 const limaBeans = new Products('Lima Beans', 'White looking beans', '', '28.85', ['beans', 'lima', 'white']);
 
 // Item Library
@@ -28,18 +31,10 @@ function searchMain() {
 function displayContent(searchContent) {
     const mainContainer = document.querySelector('.main-content');
     mainContainer.textContent = '';
-    
-    // Filter using tags
-    const filteredItems = items.filter(item => item.tags === searchContent);
-    filteredItems.forEach(item => {
-        const itemContainer = document.createElement('div');
-        itemContainer.textContent = item.title;
-        mainContainer.appendChild(itemContainer);
-    });
 
     // Filter using texts and Regex
     const regexPattern = new RegExp(searchContent, 'i');
-    const filteredItemsDescription = items.filter(item => regexPattern.test(item.tags) || regexPattern.test(item.title));
+    const filteredItemsDescription = items.filter(item => regexPattern.test(item.tags) || regexPattern.test(item.title) || regexPattern.test(item.titleColor()));
     filteredItemsDescription.forEach(item => {
         const itemContainer = document.createElement('div');
         itemContainer.textContent = item.title;
