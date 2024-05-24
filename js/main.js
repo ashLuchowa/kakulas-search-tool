55d1eb89fb14e7bef60521cccc301ac6b741471a
ashLuchowa <ashwinluchowa@gmail.com>
2024-05-24 12:07:50 +0800

    Add product class

---
+++ js/main.js
@@ -0,0 +1,10 @@
+class Products {
+    constructor(title, description, image, price, tags, id) {
+        this.title = title
+        this.description = description
+        this.image = image
+        this.price = price
+        this.tags = tags
+        this.id = id
+    }
+}
